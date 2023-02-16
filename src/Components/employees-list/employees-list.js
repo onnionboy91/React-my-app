4674d59617a59b68fbe26023a21c4eade417426f
viktor <onnionboy@yandex.ru>
2023-02-16 10:29:18 +0300

    second commit

--- src/Components/employees-list/employees-list.js
+++ src/Components/employees-list/employees-list.js
@@ -2,12 +2,16 @@ import EmployeesListItem from "../employees-list-item/employees-list-item"
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+        <EmployeesListItem {...item}/>
+        )
+    })
     return (
         <li className="app-list list-group">
-            <EmployeesListItem />
-            <EmployeesListItem />
-            <EmployeesListItem />
+            {elements}
         </li>
     )
 }
